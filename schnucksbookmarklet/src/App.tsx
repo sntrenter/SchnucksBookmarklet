e6bed5ad0b095f6856c93c2168712bb769f82bc3
Sam Trenter <sntrenter@gmail.com>
2022-09-20 18:45:55 -0500

    added a lot lol

--- schnucksbookmarklet/src/App.tsx
+++ schnucksbookmarklet/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import schnucks from "./Schnucks2.png";
+import Editor from "@monaco-editor/react";
+import "./App.css";
 
+let code = `let buttons = document.getElementsByClassName('btn item-btn bold-font schnucks-red-bg')
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].click();
+}`;
+//<img src={logo} className="App-logo" alt="logo" />
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={schnucks} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Drag this link to your bookmarks bar and click it when you are on the
+          <a target="_blank" rel="noopener noreferrer" className="App-link" href="https://nourish.schnucks.com/web-ext/coupons">Schnucks coupon page.</a>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="javascript:(function()%7Blet%20buttons%20%3D%20document.getElementsByClassName('btn%20item-btn%20bold-font%20schnucks-red-bg')%0Afor%20(let%20i%20%3D%200%3B%20i%20%3C%20buttons.length%3B%20i%2B%2B)%20%7B%0A%20%20%20%20buttons%5Bi%5D.click()%3B%0A%7D%7D)()%3B"
         >
-          Learn React
+          Schnuckscoupon 
         </a>
-      </header>
+        <p>(P.S. you really shouldn't just run random code from the internet)</p>
+      </header> 
+      <div>
+      <Editor
+     height="90vh"
+     defaultLanguage="javascript"
+     defaultValue={code}
+   />
+      </div>
     </div>
   );
 }
