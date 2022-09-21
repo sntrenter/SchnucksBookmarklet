1478acb3f4938a26ea6bc2fe13c03f5346e86233
Sam Trenter <sntrenter@gmail.com>
2022-09-20 21:33:48 -0500

    adding code

--- schnucksbookmarklet/src/App.tsx
+++ schnucksbookmarklet/src/App.tsx
@@ -16,22 +16,40 @@ function App() {
         <img src={schnucks} className="App-logo" alt="logo" />
         <p>
           Drag this link to your bookmarks bar and click it when you are on the
-          <a target="_blank" rel="noopener noreferrer" className="App-link" href="https://nourish.schnucks.com/web-ext/coupons">Schnucks coupon page.</a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link"
+            href="https://nourish.schnucks.com/web-ext/coupons"
+          >
+            Schnucks coupon page.
+          </a>
         </p>
         <a
           className="App-link"
           href="javascript:(function()%7Blet%20buttons%20%3D%20document.getElementsByClassName('btn%20item-btn%20bold-font%20schnucks-red-bg')%0Afor%20(let%20i%20%3D%200%3B%20i%20%3C%20buttons.length%3B%20i%2B%2B)%20%7B%0A%20%20%20%20buttons%5Bi%5D.click()%3B%0A%7D%7D)()%3B"
         >
-          Schnuckscoupon 
+          Schnuckscoupon
         </a>
-        <p>(P.S. you really shouldn't just run random code from the internet)</p>
-      </header> 
-      <div>
-      <Editor
-     height="90vh"
-     defaultLanguage="javascript"
-     defaultValue={code}
-   />
+        <p>
+          (P.S. you really shouldn't just run random code from the internet)
+        </p>
+      </header>
+      <div className="">
+        <Editor
+          height="90vh"
+          defaultLanguage="javascript"
+          defaultValue={code}
+          options={{
+            minimap: {
+              enabled: false,
+            },
+            readOnly: true,
+            scrollbar: {
+              vertical: "hidden",
+            },
+          }}
+        />
       </div>
     </div>
   );
